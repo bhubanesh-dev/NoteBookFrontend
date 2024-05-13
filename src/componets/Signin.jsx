8c0f3a04ca45274f88aae622173bd6e9803ad70d
bhubanesh-dev <bhubanesh.dev@gmail.com>
2024-05-13 10:10:47 +0530

    added spinner

--- src/componets/Signin.jsx
+++ src/componets/Signin.jsx
@@ -1,13 +1,24 @@
 import React, { useContext, useState } from "react";
 import host from "../Serverlink";
 
-import { useNavigate ,Link } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import userContext from "../context/user/userContext";
 
 import { useForm } from "react-hook-form";
 import { DevTool } from "@hookform/devtools";
 
+import HashLoader from "react-spinners/HashLoader";
+
+const override = {
+  display: "block",
+  margin: "0 auto",
+ 
+    
+};
+
+
 const Signin = (props) => {
+  let [loading, setLoading] = useState(false);
   const Navigate = useNavigate();
 
   const context = useContext(userContext);
@@ -23,7 +34,9 @@ const Signin = (props) => {
   };
 
   const userLogin = async (data) => {
+
     const { email, password } = data;
+    setLoading(true);
     // API Call
     const response = await fetch(`${host}/auth/login`, {
       method: "POST",
@@ -33,105 +46,111 @@ const Signin = (props) => {
       body: JSON.stringify({ email, password }),
     });
     const msg = await response.json();
+    setLoading(false);
 
-      const { message, token } = msg;
-      // Save the auth token & user info and redirect
-     
+    const { message, token } = msg;
+    // Save the auth token & user info and redirect
 
-
-      if(response.status === 403 || !msg){
-        props.showAlert(
-          "invalid credentials ",
-          "danger"
-        );
-      }
-
-     else if (response.status === 200 || msg) {
-        props.showAlert("Login successfully", "success");
-        localStorage.setItem("token", token);
-        Navigate("/");
-      } else if (response.status === 500) {
-        props.showAlert(
-          "something went wrong from our side , please report us ",
-          "danger"
-        );
-      } else if (response.status === 422) {
-        props.showAlert("plz fill the filed properly ", "warning");
-      }
-     
-    
+    if (response.status === 403 || !msg) {
+      props.showAlert("invalid credentials ", "danger");
+    } else if (response.status === 200 || msg) {
+      props.showAlert("Login successfully", "success");
+      localStorage.setItem("token", token);
+      Navigate("/");
+    } else if (response.status === 500) {
+      props.showAlert(
+        "something went wrong from our side , please report us ",
+        "danger"
+      );
+    } else if (response.status === 422) {
+      props.showAlert("plz fill the filed properly ", "warning");
+    }
   };
 
   return (
     <>
-      <div className="cointainer-fluid setCointainer">
-        <div className="row">
-          <div className="col-12 col-md-4 mx-auto  m-2 ">
-            <div className="card flex-row my-5 glassmorphism">
-              <div className="card-body p-3 p-sm-5">
-                <h5 className="card-title text-center mb-5 text-dark  fw-medium fs-5">
-                  Login
-                </h5>
-                <form
-                  method="POST"
-                  onSubmit={handleSubmit(onSubmit)}
-                  noValidate
-                >
-                  <div className="form-floating mb-3">
-                    <input
-                      type="email"
-                      className="form-control text-area"
-                      id="floatingInputEmail"
-                      {...register("email", {
-                        required: {
-                          value: true,
-                          message: "Email is  required ",
-                        },
-                        pattern: {
-                          value:
-                            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                          message: "Invalid email format",
-                        },
-                      })}
-                    />
-                    <label htmlFor="floatingInputEmail">Email address</label>
-                    <p className="errors">{errors.email?.message}</p>
-                  </div>
-
-                  <div className="form-floating mb-3">
-                    <input
-                      type="password"
-                      className="form-control text-area"
-                      id="floatingPassword"
-                      {...register("password", { required: true })}
-                    />
-                    <label htmlFor="floatingPassword">Password</label>
-                    <p className="errors">{errors.password?.message}</p>
-                  </div>
-
-                  <div className="d-grid mb-2 justify-content-center">
-                    <button
-                      className="btn btn-lg btn-primary btn-login fw-bold text-uppercase "
-                      type="submit"
-                    >
-                      Login
-                    </button>
-                  </div>
-
-                  <Link
-                    className="d-block text-center mt-2  text-dark text-decoration-none fw-medium"
-                    to="/signup"
+      {loading ? (
+        // If loading is true, render the loading spinner
+        <div className="setCointainer">
+          <HashLoader
+            color={"#101f4e"}
+            loading={loading}
+            cssOverride={override}
+            size={50}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        </div>
+      ) : (
+        // If loading is false, render the login form
+        <div className="cointainer-fluid setCointainer">
+          <div className="row">
+            <div className="col-12 col-md-4 mx-auto  m-2 ">
+              <div className="card flex-row my-5 glassmorphism">
+                <div className="card-body p-3 p-sm-5">
+                  <h5 className="card-title text-center mb-5 text-dark  fw-medium fs-5">
+                    Login
+                  </h5>
+                  <form
+                    method="POST"
+                    onSubmit={handleSubmit(onSubmit)}
+                    noValidate
                   >
-                    Don't have an account ?
-                    <span className="fw-bold"> Sign Up</span>{" "}
-                  </Link>
-                </form>
+                    <div className="form-floating mb-3">
+                      <input
+                        type="email"
+                        className="form-control text-area"
+                        id="floatingInputEmail"
+                        {...register("email", {
+                          required: {
+                            value: true,
+                            message: "Email is  required ",
+                          },
+                          pattern: {
+                            value:
+                              /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                            message: "Invalid email format",
+                          },
+                        })}
+                      />
+                      <label htmlFor="floatingInputEmail">Email address</label>
+                      <p className="errors">{errors.email?.message}</p>
+                    </div>
+
+                    <div className="form-floating mb-3">
+                      <input
+                        type="password"
+                        className="form-control text-area"
+                        id="floatingPassword"
+                        {...register("password", { required: true })}
+                      />
+                      <label htmlFor="floatingPassword">Password</label>
+                      <p className="errors">{errors.password?.message}</p>
+                    </div>
+
+                    <div className="d-grid mb-2 justify-content-center">
+                      <button
+                        className="btn btn-lg btn-primary btn-login fw-bold text-uppercase "
+                        type="submit"
+                      >
+                        Login
+                      </button>
+                    </div>
+
+                    <Link
+                      className="d-block text-center mt-2  text-dark text-decoration-none fw-medium"
+                      to="/signup"
+                    >
+                      Don't have an account ?
+                      <span className="fw-bold"> Sign Up</span>{" "}
+                    </Link>
+                  </form>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      <DevTool control={control} />
+      )}
     </>
   );
 };
--- src/componets/Signup.jsx
+++ src/componets/Signup.jsx
@@ -1,28 +1,37 @@
-import React from "react";
 import host from "../Serverlink";
+import React, { useState } from "react";
 
-import {useNavigate,Link} from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 
 import { useForm } from "react-hook-form";
 import { DevTool } from "@hookform/devtools";
 
+import HashLoader from "react-spinners/HashLoader"; 
+
 
 const Signup = (props) => {
-
-  const Navigate=useNavigate();
+  const Navigate = useNavigate();
+  let [loading, setLoading] = useState(false);
 
   const { register, handleSubmit, control, formState, getValues } = useForm();
   const { errors } = formState;
 
+  const override = {
 
+    display: "block",
+    margin: "0 auto",
+
+  };
 
   const onSubmit = (data) => {
+    
     //console.log(JSON.stringify({...data}));
     userRegister(data);
   };
   const userRegister = async (data) => {
     const { name, email, phone, password, cpassword } = data;
     // API Call
+    setLoading(true);
     const response = await fetch(`${host}/auth/register`, {
       method: "POST",
       headers: {
@@ -31,158 +40,170 @@ const Signup = (props) => {
       body: JSON.stringify({ name, email, phone, password, cpassword }),
     });
     const msg = await response.json();
-   
+    setLoading(false);
 
-
-    if(response.status === 403 || !msg){
-      props.showAlert(
-        "invalid credentials ",
-        "danger"
-      );
-    }else if (response.status === 422) {
+    if (response.status === 403 || !msg) {
+      props.showAlert("invalid credentials ", "danger");
+    } else if (response.status === 422) {
       props.showAlert("email already exist ", "warning");
-    }
-
-   else if (response.status === 201 || msg) {
+    } else if (response.status === 201 || msg) {
       props.showAlert("you are registered successfully", "success");
-      
+
       Navigate("/signin");
     } else if (response.status === 500) {
       props.showAlert(
         "something went wrong from our side , please report us ",
         "danger"
       );
-    } 
+    }
   };
   return (
     <>
-      <div className="cointainer-fluid setCointainer">
-        <div className="row">
-          <div className="col-12 col-md-4 mx-auto">
-            <div className="card flex-row my-3 glassmorphism">
-              <div className="card-body p-3 p-sm-5">
-                <h5 className="card-title text-center mb-5 text-dark  fw-medium fs-5">
-                  Register
-                </h5>
-                <form
-                  method="POST"
-                  onSubmit={handleSubmit(onSubmit)}
-                  noValidate
-                >
-                  <div className="form-floating mb-3">
-                    <input
-                      type="text"
-                      className="form-control text-area"
-                      id="floatingInputUsername"
-                      {...register("name", {
-                        required: {
-                          value: true,
-                          message: "Name required ",
-                        },
-                        minLength: {
-                          value: 5,
-                          message: "Username must be atleast 5 charecter",
-                        },
-                      })}
-                    />
-                    <label for="floatingInputUsername">Username</label>
-                    <p className="errors">{errors.name?.message}</p>
-                  </div>
-
-                  <div className="form-floating mb-3">
-                    <input
-                      type="email"
-                      className="form-control text-area"
-                      id="floatingInputEmail"
-                      {...register("email", {
-                        required: {
-                          value: true,
-                          message: "Email is  required ",
-                        },
-                        pattern: {
-                          value:
-                            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                          message: "Invalid email format",
-                        },
-                      })}
-                    />
-                    <label for="floatingInputEmail">Email address</label>
-                    <p className="errors">{errors.email?.message}</p>
-                  </div>
-
-                  <div className="form-floating mb-3">
-                    <input
-                      type="tel"
-                      className="form-control text-area"
-                      id="floatingInputPhone"
-                      {...register("phone", {
-                        required: {
-                          value: true,
-                          message: "Phone number required ",
-                        },
-                        minLength: {
-                          value: 10,
-                          message: "Invalid number",
-                        },
-                        maxLength: {
-                          value: 10,
-                          message: "Invalid number",
-                        },
-                      })}
-                    />
-                    <label for="floatingInputPhone">Phone</label>
-                    <p className="errors">{errors.phone?.message}</p>
-                  </div>
-
-                  <div className="form-floating mb-3">
-                    <input
-                      type="password"
-                      className="form-control text-area"
-                      id="floatingPassword"
-                      {...register("password", { required: true })}
-                    />
-                    <label for="floatingPassword">Password</label>
-                  </div>
-
-                  <div className="form-floating mb-3">
-                    <input
-                      type="password"
-                      className="form-control text-area"
-                      id="floatingPasswordConfirm"
-                      {...register("cpassword", { required: true })}
-                    />
-                    <label for="floatingPasswordConfirm">
-                      Confirm Password
-                    </label>
-                    <p className="errors">
-                      {getValues("cpassword") !== getValues("password")
-                        ? "Password don't match"
-                        : true}
-                    </p>
-                  </div>
-
-                  <div className="d-grid mb-2 justify-content-center">
-                    <button
-                      className="btn btn-lg btn-primary btn-login fw-bold text-uppercase "
-                      type="submit"
-                    >
+      {loading ? (
+        // If loading is true, render the loading spinner
+        <div className="setCointainer">
+          <HashLoader
+            color={"#101f4e"}
+            loading={loading}
+            cssOverride={override}
+            size={50}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        </div>
+      ) : (
+        // If loading is false, render the login form
+        <>
+          <div className="cointainer-fluid setCointainer">
+            <div className="row">
+              <div className="col-12 col-md-4 mx-auto">
+                <div className="card flex-row my-3 glassmorphism">
+                  <div className="card-body p-3 p-sm-5">
+                    <h5 className="card-title text-center mb-5 text-dark  fw-medium fs-5">
                       Register
-                    </button>
-                  </div>
+                    </h5>
+                    <form
+                      method="POST"
+                      onSubmit={handleSubmit(onSubmit)}
+                      noValidate
+                    >
+                      <div className="form-floating mb-3">
+                        <input
+                          type="text"
+                          className="form-control text-area"
+                          id="floatingInputUsername"
+                          {...register("name", {
+                            required: {
+                              value: true,
+                              message: "Name required ",
+                            },
+                            minLength: {
+                              value: 5,
+                              message: "Username must be atleast 5 charecter",
+                            },
+                          })}
+                        />
+                        <label for="floatingInputUsername">Username</label>
+                        <p className="errors">{errors.name?.message}</p>
+                      </div>
 
-                  <Link
-                    className="d-block text-center mt-2  text-dark text-decoration-none fw-medium"
-                    to="/signin"
-                  >
-                    Have an account?<span className="fw-bold"> Sign In</span>{" "}
-                  </Link>
-                </form>
+                      <div className="form-floating mb-3">
+                        <input
+                          type="email"
+                          className="form-control text-area"
+                          id="floatingInputEmail"
+                          {...register("email", {
+                            required: {
+                              value: true,
+                              message: "Email is  required ",
+                            },
+                            pattern: {
+                              value:
+                                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                              message: "Invalid email format",
+                            },
+                          })}
+                        />
+                        <label for="floatingInputEmail">Email address</label>
+                        <p className="errors">{errors.email?.message}</p>
+                      </div>
+
+                      <div className="form-floating mb-3">
+                        <input
+                          type="tel"
+                          className="form-control text-area"
+                          id="floatingInputPhone"
+                          {...register("phone", {
+                            required: {
+                              value: true,
+                              message: "Phone number required ",
+                            },
+                            minLength: {
+                              value: 10,
+                              message: "Invalid number",
+                            },
+                            maxLength: {
+                              value: 10,
+                              message: "Invalid number",
+                            },
+                          })}
+                        />
+                        <label for="floatingInputPhone">Phone</label>
+                        <p className="errors">{errors.phone?.message}</p>
+                      </div>
+
+                      <div className="form-floating mb-3">
+                        <input
+                          type="password"
+                          className="form-control text-area"
+                          id="floatingPassword"
+                          {...register("password", { required: true })}
+                        />
+                        <label for="floatingPassword">Password</label>
+                      </div>
+
+                      <div className="form-floating mb-3">
+                        <input
+                          type="password"
+                          className="form-control text-area"
+                          id="floatingPasswordConfirm"
+                          {...register("cpassword", { required: true })}
+                        />
+                        <label for="floatingPasswordConfirm">
+                          Confirm Password
+                        </label>
+                        <p className="errors">
+                          {getValues("cpassword") !== getValues("password")
+                            ? "Password don't match"
+                            : true}
+                        </p>
+                      </div>
+
+                      <div className="d-grid mb-2 justify-content-center">
+                        <button
+                          className="btn btn-lg btn-primary btn-login fw-bold text-uppercase "
+                          type="submit"
+                        >
+                          Register
+                        </button>
+                      </div>
+
+                      <Link
+                        className="d-block text-center mt-2  text-dark text-decoration-none fw-medium"
+                        to="/signin"
+                      >
+                        Have an account?
+                        <span className="fw-bold"> Sign In</span>{" "}
+                      </Link>
+                    </form>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </div>
-      <DevTool control={control} />
+          <DevTool control={control} />
+        </>
+      )}
     </>
   );
 };
